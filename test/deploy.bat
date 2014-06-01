1231e21a198fd23e02a9d2d2c716431a2bfcd9e2
densl <zengpush@gmail.com>
2014-06-01 10:55:37 +0800

    update auto bat

--- test/deploy.bat
+++ test/deploy.bat
@@ -1,6 +1,7 @@
 @echo off
 set destDir=d:\software\GreenSoftware\apache-tomcat-7.0.54\webapps\test
-set srcDir=test
+set srcDir=f:\GitHub\javassh\test
+
 
 rmdir %destDir% /s /q
 cp %srcDir% %destDir% -r
